9d391a3a293211847db337ca702f93acd7eb5c2a
jcaluiz <lacjunior129.la@gmail.com>
2023-02-11 03:45:51 -0300

    renderização dos pokemons e colocando o input

--- src/components/PokemonComponent/PokemonComponent.vue
+++ src/components/PokemonComponent/PokemonComponent.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="pokemon-container">
-    <h1>Componente do Pokemon</h1>
+    <img :src="imagePokemon" alt="`Imagem de ${namePokemon}`" />
+    <p>{{ namePokemon }}</p>
+    <!-- <p>{{ feature[0].value }}</p> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "PokemonComponent",
+  props: {
+    namePokemon: String,
+    imagePokemon: String,
+    // feature: Array,
+  },
 };
 </script>
+
+<style src="./PokemonComponent.scss" lang="scss" scoped></style>
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
   <main id="home-container">
     <h1>Pokemon</h1>
+    <input type="text" v-model="value" />
     <section id="pokemons-home-container">
-      <PokemonComponent />
+      <PokemonComponent
+        v-for="(pokemon, index) in getPokemons"
+        :key="index"
+        :namePokemon="pokemon.name"
+        :imagePokemon="pokemon.image"
+        :feature="pokemon.feature"
+      />
     </section>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import PokemonComponent from "../../components/PokemonComponent/PokemonComponent.vue";
 
 export default {
@@ -15,10 +23,54 @@ export default {
   data() {
     return {
       getPokemons: [],
+      staticPokemons: [],
+      value: "",
     };
   },
   components: {
     PokemonComponent,
+  },
+  methods: {
+    formatPokemonsObjectInArray(pokemonList) {
+      const backDefault = "back_default";
+      pokemonList.map(({ name, url }) =>
+        axios(url)
+          .then((response) => {
+            const { sprites, stats } = response.data;
+            const pokemons = [
+              ...this.getPokemons,
+              {
+                name,
+                image: sprites[backDefault],
+                feature: stats.map((item) => ({
+                  nameFeature: item.stat.name,
+                  value: item["base_stat"],
+                })),
+              },
+            ];
+            this.getPokemons = pokemons;
+            this.staticPokemons = pokemons;
+          })
+          .catch((error) => console.log(error))
+      );
+    },
+    fetchPokemonsInformations() {
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/")
+        .then((response) => {
+          const { results } = response.data;
+          this.formatPokemonsObjectInArray(results);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    this.fetchPokemonsInformations();
+  },
+  beforeUpdate() {
+    this.getPokemons = this.staticPokemons.filter(({ name }) =>
+      name.includes(this.value)
+    );
   },
 };
 </script>
