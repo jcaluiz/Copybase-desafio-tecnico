f1c6a3e53574ee78e5479c5e4403cd4b3e9644c4
jcaluiz <lacjunior129.la@gmail.com>
2023-02-11 00:35:30 -0300

    Criação do componente PokemonComponent para receber as característica de Pokemon

---
+++ src/components/PokemonComponent/PokemonComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pokemon-container">
+    <h1>Componente do Pokemon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonComponent",
+};
+</script>
---
+++ src/views/HomeView/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main id="home-container">
+    <h1>Pokemon</h1>
+    <section id="pokemons-home-container">
+      <PokemonComponent />
+    </section>
+  </main>
+</template>
+
+<script>
+import PokemonComponent from "../../components/PokemonComponent/PokemonComponent.vue";
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      getPokemons: [],
+    };
+  },
+  components: {
+    PokemonComponent,
+  },
+};
+</script>
+
+<style src="./HomeView.scss" lang="scss" scoped></style>
